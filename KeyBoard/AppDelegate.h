eee8278a7bf41c3a012e3c58cbc4722fdd5b787c
朱明灿 <zhumingcan@zhumingcandeimac.local>
2017-02-25 16:46:53 +0800

    Initial Commit

---
+++ KeyBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyBoard
+//
+//  Created by 朱明灿 on 17/2/25.
+//  Copyright © 2017年 张佳强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
